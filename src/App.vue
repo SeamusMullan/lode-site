e3b6a77b890823093c8da198973226e3c6e17403
Seamus Mullan <seamusmullan2023@gmail.com>
2025-08-18 19:34:31 +0100

    add basic ahh header with logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
+
 <script setup>
+import Header from './components/Header.vue';
 </script>
 
 <template>
+  <div class="app">
+    <Header />
+    <!-- Contains RouterLink components -->
+    <main class="content">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <!-- <h1>My Website</h1> -->
+       <img src="../../public/lode.png" alt="Logo" style="height:5rem; width: auto;">
+    </div>
+
+    <div class="header-right">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/portfolio">About</RouterLink>
+      <RouterLink to="/testimonials">Testimonials</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 1.000) 0%, // full black
+    rgba(0, 0, 0, 0.738) 19%,
+    rgba(0, 0, 0, 0.541) 34%,
+    rgba(0, 0, 0, 0.382) 47%,
+    rgba(0, 0, 0, 0.278) 56.5%,
+    rgba(0, 0, 0, 0.194) 65%,
+    rgba(0, 0, 0, 0.126) 73%,
+    rgba(0, 0, 0, 0.075) 80.2%,
+    rgba(0, 0, 0, 0.042) 86.1%,
+    rgba(0, 0, 0, 0.021) 91%,
+    rgba(0, 0, 0, 0.008) 95.2%,
+    rgba(0, 0, 0, 0.002) 98.2%,
+    transparent 100%
+  );
+  padding: 4rem;
+
+  h1 {
+    margin: 0;
+    font-size: 1.5rem;
+  }
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+
+.header-left {
+    display: flex;
+    align-items: center;
+    
+    h1 {
+        margin: 0;
+        font-size: 2.5rem;
+    }
+}
+
+.header-right {
+  display: flex;
+  gap: 1rem;
+  padding: 0rem 2rem;
+
+  a,
+  RouterLink {
+    color: white;
+    text-decoration: none;
+    font-size: 1.8rem;
+
+    &:hover {
+      text-decoration: underline;
+      cursor: pointer;
+    }
+  }
+}
+
+</style>
